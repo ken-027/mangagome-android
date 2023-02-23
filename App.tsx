377f17dd492bc4d109945a31f544d40e4bb0f897
ken <82031953+ken-027@users.noreply.github.com>
2023-02-23 22:31:46 +0800

    minor changes

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 // import type {PropsWithChildren} from 'react';
 import {
   // SafeAreaView,
@@ -13,34 +13,74 @@ import {
   // StatusBar,
   StyleSheet,
   Text,
+  // Image,
   // useColorScheme,
+  Pressable,
+  Linking,
   View,
-} from 'react-native';
-import {WebView} from 'react-native-webview';
+} from 'react-native'
+import { WebView } from 'react-native-webview'
 
-function App() {
+const ErrorPreview: React.FC<{ onReload: any }> = ({ onReload }) => {
   return (
-    <View style={styles.container}>
-      <WebView
-        renderError={error => <Text>{error}</Text>}
-        onError={error => console.log(error)}
-        renderLoading={() => <Text>...</Text>}
-        source={{uri: 'https://www.mangago.me/'}}
-      />
-      <Text style={styles.text}>
-        a website to web app created by: <Text style={styles.name}>@ken</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={styles.errorText}>
+        An error occurred while loading the page.
       </Text>
+      <Pressable onPress={onReload} style={styles.button}>
+        <Text style={styles.buttonText}>Refresh</Text>
+      </Pressable>
     </View>
-  );
+  )
+}
+
+const App = () => {
+  const [error, setError] = useState<unknown>()
+  const [reload, setReload] = useState<boolean>()
+
+  useEffect(() => {
+    console.log('refresh')
+    console.log(error)
+  }, [reload])
+
+  return (
+    <>
+      {error ? (
+        <ErrorPreview
+          onReload={() => {
+            setReload((prevState) => !prevState)
+            setError(null)
+          }}
+        />
+      ) : (
+        <View style={styles.container}>
+          <WebView
+            pullToRefreshEnabled
+            renderError={(error) => <Text>{error}</Text>}
+            onError={(errorPage) => setError(errorPage)}
+            renderLoading={() => <Text>...</Text>}
+            source={{ uri: 'https://www.mangago.me/' }}
+          />
+          <Text style={styles.text}>
+            powered by mangago.me and created by:{' '}
+            <Text
+              style={styles.name}
+              onPress={() =>
+                Linking.openURL('https://kenneth-andales.github.io')
+              }>
+              @ken
+            </Text>
+          </Text>
+        </View>
+      )}
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    marginTop: 18,
     justifyContent: 'center',
-    // paddingTop: 10,
   },
   sectionContainer: {
     marginTop: 32,
@@ -58,19 +98,32 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+  errorText: {
+    fontSize: 20,
+  },
   text: {
-    backgroundColor: '#ededed',
-    borderTopColor: '#5c5a56',
+    backgroundColor: '#5c5a56',
+    borderTopColor: '#787571',
     borderTopWidth: 1,
-    color: '#5c5a56',
+    color: '#ededed',
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 14,
     padding: 5,
   },
   name: {
-    color: '#cf6c30',
+    color: '#34c95c',
   },
-});
+  button: {
+    backgroundColor: '#5c5a56',
+    marginTop: 20,
+    paddingHorizontal: 30,
+    paddingVertical: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#ededed',
+  },
+})
 
-export default App;
+export default App
